3b813cb481653c9863ce65207053b8ee87f0e197
francielli <francielli0603@gmail.com>
2025-04-13 23:30:34 -0300

    Fran - ajuste de brilho na foto

---
+++ Fran/brightness.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+import numpy as np
+
+imagem = cv2.imread('imagens/0/Anger.jpg')
+imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+
+brilho_medio = np.mean(imagemCinza)
+print(f'Brilho médio: {brilho_medio:.2f}')
+
+if brilho_medio < 100:
+    ajuste_brilho = 50 
+elif brilho_medio > 180:
+    ajuste_brilho = -50 
+else:
+    ajuste_brilho = 0 
+
+imagemTransformada = cv2.convertScaleAbs(imagem, alpha=1, beta=ajuste_brilho)
+pastaDestino = 'imagensFormatadas'
+os.makedirs(pastaDestino, exist_ok=True)
+pastaTransformada = os.path.join(pastaDestino, 'imagem_com_brilho.jpg')
+cv2.imwrite(pastaTransformada, imagemTransformada)
+print(f'Imagem salva em: {pastaTransformada}')
